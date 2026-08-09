73abce09f0a43041f864b2b947b7dabd2d1a49a8
Flavio Domiziano Gualtieri <flavio.dom.gualtieri@gmail.com>
2026-08-09 18:32:32 +0100

    ready for fist mph run

--- src/cloudforger/vectorizers/__init__.py
+++ src/cloudforger/vectorizers/__init__.py
@@ -1,10 +1,13 @@
 from .persistence_image import PersistenceImager, linear_weight
 from .multi_channel import MultiChannelImager
 from .calibrated import build_calibrated_imager
+from .signed_measure_image import SignedMeasureImager, MultiDegreeSignedMeasureImager
 
 __all__ = [
     "PersistenceImager",
     "linear_weight",
     "MultiChannelImager",
     "build_calibrated_imager",
+    "SignedMeasureImager",
+    "MultiDegreeSignedMeasureImager",
 ]
